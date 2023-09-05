318e9e97004f55c9840158d2608d2571cee19a1d
May Pyone <maypyone015@gmail.com>
2023-09-06 03:23:25 +0630

    Add the add action

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,13 +1,30 @@
 import './styles/form.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../redux/book/booksSlice';
 
 export default function Form() {
+  const dispatch = useDispatch();
+  const { totalbooks } = useSelector((state) => state.book);
+  const book = () => {
+    const tit = document.querySelector('.title').value;
+    const auth = document.querySelector('.author').value;
+    const i = totalbooks.length + 1;
+    const b = {
+      title: tit,
+      author: auth,
+      item_id: `item${i}`,
+    };
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+    return b;
+  };
   return (
     <form>
       <span>Add new book</span>
       <div>
-        <input placeholder="book title" />
-        <input placeholder="author" />
-        <button type="button">ADD BOOK</button>
+        <input className="title" placeholder="book title" />
+        <input className="author" placeholder="author" />
+        <button type="button" onClick={() => dispatch(addBook(book()))}>ADD BOOK</button>
       </div>
     </form>
   );
