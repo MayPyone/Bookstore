8c455f37f1da93245503f6b385c3fc304c4b3b5c
May Pyone <maypyone015@gmail.com>
2023-09-08 02:07:40 +0630

    Delete showing category

--- src/component/Book.js
+++ src/component/Book.js
@@ -4,13 +4,12 @@ import { deleteUsers } from '../redux/book/booksSlice';
 
 export default function Book(prop) {
   const {
-    category, title, author, item_id,
+    title, author, item_id,
   } = prop;
   const dispatch = useDispatch();
   return (
     <li className="list-item">
       <div key={item_id}>
-        <div>{category}</div>
         <div>{title}</div>
         <div>{author}</div>
         <button type="button" onClick={() => { dispatch(deleteUsers(item_id)); }}>Remove</button>
